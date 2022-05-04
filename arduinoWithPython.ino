c903e3deefd6257b46787f177200ac9471639d4a
sho96 <81459356+sho96@users.noreply.github.com>
2022-05-04 11:31:52 +0900

    Add files via upload

---
+++ arduinoWithPython.ino
@@ -0,0 +1,81 @@
+String inputBuffer;
+
+String cmds[3] = {};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(500000);
+  /*
+  int index = split("pinMode 3 OUTPUT", ' ', cmds);
+  for(int i = 0; i < index; i++){
+    Serial.println(cmds[i]);
+  }*/
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    char received = Serial.read();
+    if(received == '\n'){
+      //Serial.println(inputBuffer);
+      String cmds[3] = {};
+      int index = split(inputBuffer, ' ', cmds);
+      for(int i = 0; i < index; i++){
+        Serial.println(cmds[i]);
+      }
+      if(cmds[0] == "pinMode"){
+        if(cmds[2] == "OUTPUT"){
+          pinMode(cmds[1].toInt(), OUTPUT);
+          //Serial.println("output set");
+        }
+        if(cmds[2] == "INPUT"){
+          pinMode(cmds[1].toInt(), INPUT);
+        }
+        if(cmds[2] == "INPUT_PULLUP"){
+          pinMode(cmds[1].toInt(), INPUT_PULLUP);
+        }
+      }
+      if(cmds[0] == "digitalWrite"){
+        digitalWrite(cmds[1].toInt(), cmds[2] == "true");
+      }
+      if(cmds[0] == "analogWrite"){
+        analogWrite(cmds[1].toInt(), cmds[2].toInt());
+      }
+      if(cmds[0] == "digitalRead"){
+        if(digitalRead(cmds[1].toInt())){
+          sendData("1");
+        }else{
+          sendData("0");
+        }
+      }
+      if(cmds[0] == "analogRead"){
+        sendData(String(analogRead(cmds[1].toInt())));
+      }
+      inputBuffer = "";
+    }else{
+      inputBuffer += received;
+    }
+  }
+}
+
+void sendData(String value){
+  int valueSize = value.length() / sizeof(String);
+  Serial.print(String(valueSize)+"a");
+  Serial.print(value);
+}
+
+int split(String data, char delimiter, String *dst){
+  int index = 0;
+  int arraySize = (sizeof(data))/sizeof((data[0]));
+  int datalength = data.length();
+  
+  for(int i = 0; i < datalength; i++){
+    char tmp = data.charAt(i);
+    if( tmp == delimiter ){
+      index++;
+      if( index > (arraySize - 1)) return -1;
+    }
+    else dst[index] += tmp;
+  }
+  return (index + 1);
+}
