16e54609c173138a6dd36c44726db633d343fe98
sho96 <81459356+sho96@users.noreply.github.com>
2022-05-04 10:14:57 +0900

    Update arduinoController.py

--- arduinoController.py
+++ arduinoController.py
@@ -4,14 +4,17 @@ import time
 class Controller:
     pinmodes = ["OUTPUT", "INPUT", "INPUT_PULLUP"]
     def __init__(self, portid, baud = 500000, waitTime = 5):
+        #connect to arduino
         print("connecting...")
         self.ser = serial.Serial(portid, baudrate = baud, timeout = 1)
         #self.ser.open()
         time.sleep(waitTime)
         print("connection established")
     def __del__(self):
+        #close port when deleted
         self.ser.close()
     def readValue(self):
+        #code to receive a data from arduino
         datasize = b""
         while True:
             byte = self.ser.read()
@@ -23,11 +26,13 @@ class Controller:
                 datasize += byte
         return self.ser.read(size = datasize)
     def pinMode(self, pin, mode):
+        #set specific pin to some pin mode
         if not mode in self.pinmodes:
             self.ser.close()
             raise ValueError(f"pin mode named: {mode} is not supported\nuse one of {pinmodes}")
         self.ser.write(f"pinMode {pin} {mode}\n".encode("ascii"))
     def digitalWrite(self, pin, state):
+        #turn a pin on or off
         if not isinstance(state, bool):
             self.ser.close()
             raise ValueError("state only supports boolean values")
@@ -37,16 +42,20 @@ class Controller:
             state = "false"
         self.ser.write(f"digitalWrite {pin} {state}\n".encode("ascii"))
     def analogWrite(self, pin, value):
+        #output pwm from a pin
         if not isinstance(value, int):
             self.ser.close()
             raise ValueError("value only supports integer values")
         self.ser.write(f"analogWrite {pin} {value}\n".encode("ascii"))
     def digitalRead(self, pin):
+        #read a on off value from a pin
         self.ser.write(f"digitalRead {pin}\n".encode("ascii"))
         return bool(int(self.readValue().decode("ascii")))
     def analogRead(self, pin):
+        #read an analog value on a analog input pin
         self.ser.write(f"analogRead {pin}\n".encode("ascii"))
         return int(self.readValue())
     def close(self):
+        #close serial port
         self.ser.close()
     
