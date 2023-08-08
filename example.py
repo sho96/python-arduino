013b137ce619cc3d3df85d1cedd80f8664cb7a91
sho96 <81459356+sho96@users.noreply.github.com>
2023-08-08 21:33:50 +0900

    Update example.py

--- example.py
+++ example.py
@@ -5,12 +5,19 @@ controller = arduino.Controller("COM5") #/dev/tty/... for linux/mac
 
 buttonPin = 4
 ledPin = 3
+quitButton = 5
 
 controller.pinMode(ledPin, "OUTPUT")
 controller.pinMode(buttonPin, "INPUT") #INPUT_PULLUP can also be used
+controller.pinMode(quitButton, "INPUT")
 
 while True:
     pinState = controller.digitalRead(buttonPin)
     controller.digitalWrite(ledPin, not pinState)
-    #※cannot put more than two arduino functions into another arduino function:
-    #ex: controller.digitalWrite(ledPin, controller.digitalRead(buttonPin))
+    ### cannot put more than two arduino functions into another arduino function###
+    ### ex: controller.digitalWrite(ledPin, controller.digitalRead(buttonPin)) ###
+
+    if controller.digitalRead(quitButton):
+        break
+
+controller.close()
